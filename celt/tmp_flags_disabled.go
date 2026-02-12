0a57c976a5873955e9ab414c3441d120330f5dd1
thesyncim <thesyncim@gmail.com>
2026-02-12 16:29:13 +0000

    refactor(celt): compile out tmp env branches in encode prefilter path

---
+++ celt/tmp_flags_disabled.go
@@ -0,0 +1,27 @@
+//go:build !gopus_tmp_env
+
+package celt
+
+// Production/default build: compile out temporary debug/tuning branches.
+const tmpSkipMDCTHistRoundEnabled = false
+const tmpQuantInputLSBEnabled = false
+const tmpDisableDCRejectEnabled = false
+const tmpDisablePrefilterEnabled = false
+const tmpForceMaxPitchRatio1Enabled = false
+const tmpDumpXB19Enabled = false
+const tmpSkipPrefOutRoundEnabled = false
+const tmpDumpMDCT56Enabled = false
+const tmpEnergyInputF32Enabled = false
+const tmpDumpNorm56Enabled = false
+const tmpRoundNormF32Enabled = false
+const tmpDumpNormEnabled = false
+const tmpTrimUseAnalysisEnabled = false
+const tmpTrimDebugEnabled = false
+const tmpPVQDump56Enabled = false
+const tmpPVQCallDebugEnabled = false
+const tmpTHDebugEnabled = false
+const tmpQDebugEnabled = false
+const tmpSkipPrefInputRoundEnabled = false
+const tmpPrefilterF64Enabled = false
+const tmpPrefCombDumpEnabled = false
+const tmpSkipPrefMemRoundEnabled = false
