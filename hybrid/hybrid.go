1e70e66154b09a4cdb73408c08a8a77b62edde6d
thesyncim <thesyncim@gmail.com>
2026-04-11 22:27:31 +0100

    refactor: inline decode reset

--- hybrid/hybrid.go
+++ hybrid/hybrid.go
@@ -139,7 +139,8 @@ func (d *Decoder) DecodeWithDecoderHook(rd *rangecoding.Decoder, frameSize int, 
 		return nil, err
 	}
 
-	d.finishSuccessfulDecode(frameSize, d.channels)
+	d.plcState.Reset()
+	d.plcState.SetLastFrameParams(plc.ModeHybrid, frameSize, d.channels)
 
 	return samples, nil
 }
@@ -160,14 +161,10 @@ func (d *Decoder) decodePacket(data []byte, frameSize int, packetStereo bool, la
 		return nil, err
 	}
 
-	d.finishSuccessfulDecode(frameSize, lastFrameChannels)
+	d.plcState.Reset()
+	d.plcState.SetLastFrameParams(plc.ModeHybrid, frameSize, lastFrameChannels)
 
 	return samples, nil
-}
-
-func (d *Decoder) finishSuccessfulDecode(frameSize, channels int) {
-	d.plcState.Reset()
-	d.plcState.SetLastFrameParams(plc.ModeHybrid, frameSize, channels)
 }
 
 // DecodeStereoWithDecoder decodes stereo using a pre-initialized range decoder.
