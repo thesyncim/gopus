7d5dae96e70b83cb14a02a7448b487ab49303bf6
thesyncim <thesyncim@gmail.com>
2026-04-11 19:18:46 +0100

    refactor: isolate allocation helpers

--- celt/decoder.go
+++ celt/decoder.go
@@ -69,86 +69,17 @@ func (d *Decoder) DecodeFrame(data []byte, frameSize int) ([]float64, error) {
 	energies := d.decodeCoarseEnergyInto(ensureFloat64Slice(&d.scratchEnergies, end*d.channels), end, intra, lm)
 	traceRange("coarse", rd)
 
-	tfRes := ensureIntSlice(&d.scratchTFRes, end)
-	tfDecode(start, end, transient, tfRes, lm, rd)
-	traceRange("tf", rd)
-
-	spread := spreadNormal
-	tell = rd.Tell()
-	if tell+4 <= totalBits {
-		spread = rd.DecodeICDF(spreadICDF, 5)
-	}
-	traceFlag("spread", spread)
-	traceRange("spread", rd)
-
-	cap := ensureIntSlice(&d.scratchCaps, end)
-	initCapsInto(cap, end, lm, d.channels)
-	offsets := ensureIntSlice(&d.scratchOffsets, end)
-	dynallocLogp := 6
-	totalBitsQ3 := totalBits << bitRes
-	tellFrac := rd.TellFrac()
-	for i := start; i < end; i++ {
-		width := d.channels * (EBands[i+1] - EBands[i]) << lm
-		quanta := min(width<<bitRes, max(6<<bitRes, width))
-		dynallocLoopLogp := dynallocLogp
-		boost := 0
-		j := 0
-		for ; tellFrac+(dynallocLoopLogp<<bitRes) < totalBitsQ3 && boost < cap[i]; j++ {
-			flag := rd.DecodeBit(uint(dynallocLoopLogp))
-			tellFrac = rd.TellFrac()
-			if flag == 0 {
-				break
-			}
-			boost += quanta
-			totalBitsQ3 -= quanta
-			dynallocLoopLogp = 1
-		}
-		offsets[i] = boost
-		traceAllocation(i, boost, -1)
-		if j > 0 {
-			dynallocLogp = max(2, dynallocLogp-1)
-		}
-	}
-	traceRange("dynalloc", rd)
-
-	allocTrim := 5
-	encodedTrim := tellFrac+(6<<bitRes) <= totalBitsQ3
-	if encodedTrim {
-		allocTrim = rd.DecodeICDF(trimICDF, 7)
-	}
-	traceFlag("alloc_trim", allocTrim)
-	traceRange("trim", rd)
-
-	bitsQ3 := (totalBits << bitRes) - rd.TellFrac() - 1
-	antiCollapseRsv := 0
-	if transient && lm >= 2 && bitsQ3 >= (lm+2)<<bitRes {
-		antiCollapseRsv = 1 << bitRes
-	}
-	bitsQ3 -= antiCollapseRsv
-
-	pulses := ensureIntSlice(&d.scratchPulses, end)
-	fineQuant := ensureIntSlice(&d.scratchFineQuant, end)
-	finePriority := ensureIntSlice(&d.scratchFinePriority, end)
-	intensity := 0
-	dualStereo := 0
-	balance := 0
-	allocScratch := d.allocationScratch()
-	codedBands := cltComputeAllocationWithScratch(start, end, offsets, cap, allocTrim, &intensity, &dualStereo,
-		bitsQ3, &balance, pulses, fineQuant, finePriority, d.channels, lm, rd, allocScratch)
-	traceRange("alloc", rd)
-
-	for i := start; i < end; i++ {
-		width := 0
-		if i+1 < len(EBands) {
-			width = (EBands[i+1] - EBands[i]) << lm
-		}
-		k := 0
-		if width > 0 {
-			k = bitsToK(pulses[i], width)
-		}
-		traceAllocation(i, pulses[i], k)
-		traceFineBits(i, fineQuant[i])
-	}
+	allocation := d.decodeBandAllocation(rd, totalBits, start, end, lm, transient)
+	tfRes := allocation.tfRes
+	spread := allocation.spread
+	antiCollapseRsv := allocation.antiCollapseRsv
+	pulses := allocation.pulses
+	fineQuant := allocation.fineQuant
+	finePriority := allocation.finePriority
+	intensity := allocation.intensity
+	dualStereo := allocation.dualStereo
+	balance := allocation.balance
+	codedBands := allocation.codedBands
 
 	d.DecodeFineEnergy(energies, end, fineQuant)
 	qext := d.prepareQEXTDecode(qextPayload, rd, end, lm, frameSize)
