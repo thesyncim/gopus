5bb2d7b9d0c12557461ed2f29a5e51c58ee07b24
thesyncim <thesyncim@gmail.com>
2026-05-05 19:55:39 +0100

    Trim CELT tone LPC correlation bounds

--- celt/tone_lpc_corr_default.go
+++ celt/tone_lpc_corr_default.go
@@ -4,11 +4,13 @@ package celt
 
 // toneLPCCorr computes three float32 correlations for toneLPC.
 func toneLPCCorr(x []float32, cnt, delay, delay2 int) (r00, r01, r02 float32) {
-	for i := 0; i < cnt; i++ {
-		xi := x[i]
+	x0 := x[:cnt:cnt]
+	x1 := x[delay : delay+cnt : delay+cnt]
+	x2 := x[delay2 : delay2+cnt : delay2+cnt]
+	for i, xi := range x0 {
 		r00 += xi * xi
-		r01 += xi * x[i+delay]
-		r02 += xi * x[i+delay2]
+		r01 += xi * x1[i]
+		r02 += xi * x2[i]
 	}
 	return
 }
