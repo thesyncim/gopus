e397ac5abe9cacb175719cc570a305a252142dc4
thesyncim <thesyncim@gmail.com>
2026-05-19 13:33:00 +0100

    Expand DRED payload byte parity matrix

--- internal/dred/encode_libopus_parity_test.go
+++ internal/dred/encode_libopus_parity_test.go
@@ -146,3 +146,75 @@ func TestEncodeExperimentalPayloadMatchesLibopusLargeLaplaceContinuation(t *test
 		t.Fatalf("lastExtraDREDOffset=%d want %d", lastExtra, want.LastExtraDREDOffset)
 	}
 }
+
+func TestEncodeExperimentalPayloadMatchesLibopusMatrix(t *testing.T) {
+	for _, tc := range []struct {
+		name        string
+		q0          int
+		dQ          int
+		qmax        int
+		maxChunks   int
+		maxBytes    int
+		latentsFill int
+		dredOffset  int
+		latentOff   int
+		lastExtraIn int
+		activeStart int
+		activeEnd   int
+		seed        int
+	}{
+		{name: "qmax_limited", q0: 5, dQ: 2, qmax: 10, maxChunks: 5, maxBytes: MaxDataSize, latentsFill: 10, dredOffset: 14, activeStart: 0, activeEnd: 64, seed: 3},
+		{name: "zero_delta", q0: 12, dQ: 0, qmax: 12, maxChunks: 4, maxBytes: MaxDataSize, latentsFill: 8, dredOffset: 16, activeStart: 0, activeEnd: 48, seed: 5},
+		{name: "extended_offset", q0: 6, dQ: 3, qmax: 15, maxChunks: 6, maxBytes: MaxDataSize, latentsFill: 10, dredOffset: 0, activeStart: 24, activeEnd: 80, seed: 7},
+		{name: "high_q0", q0: 14, dQ: 1, qmax: 15, maxChunks: 4, maxBytes: MaxDataSize, latentsFill: 8, dredOffset: 12, activeStart: 0, activeEnd: 64, seed: 11},
+		{name: "max_bytes_suppressed", q0: 6, dQ: 5, qmax: 15, maxChunks: 8, maxBytes: 1, latentsFill: 8, dredOffset: 12, activeStart: 0, activeEnd: 64, seed: 13},
+	} {
+		t.Run(tc.name, func(t *testing.T) {
+			var state [MaxFrames * StateDim]float32
+			var latents [MaxFrames * LatentDim]float32
+			var activity [ActivityHistorySize]byte
+			fillDREDEncodeParityHistory(state[:], latents[:], tc.seed)
+			for i := tc.activeStart; i < tc.activeEnd && i < len(activity); i++ {
+				activity[i] = 1
+			}
+
+			want, err := probeLibopusDREDEncodePayload(tc.q0, tc.dQ, tc.qmax, tc.maxChunks, tc.maxBytes, tc.latentsFill, tc.dredOffset, tc.latentOff, tc.lastExtraIn, state[:], latents[:], activity)
+			if err != nil {
+				t.Fatalf("probeLibopusDREDEncodePayload() error: %v", err)
+			}
+
+			payload := make([]byte, tc.maxBytes+ExperimentalHeaderBytes)
+			lastExtra := tc.lastExtraIn
+			n := EncodeExperimentalPayload(payload, tc.maxChunks, tc.q0, tc.dQ, tc.qmax, state[:], latents[:], tc.latentsFill, tc.dredOffset, tc.latentOff, &lastExtra, activity[:])
+			if n != expectedExperimentalPayloadLength(len(want.Payload)) {
+				t.Fatalf("EncodeExperimentalPayload()=%d want %d", n, expectedExperimentalPayloadLength(len(want.Payload)))
+			}
+			gotPayload := []byte(nil)
+			if n > 0 {
+				gotPayload = payload[ExperimentalHeaderBytes:n]
+			}
+			if !bytes.Equal(gotPayload, want.Payload) {
+				t.Fatalf("payload mismatch\ngot=%x\nwant=%x", gotPayload, want.Payload)
+			}
+			if lastExtra != want.LastExtraDREDOffset {
+				t.Fatalf("lastExtraDREDOffset=%d want %d", lastExtra, want.LastExtraDREDOffset)
+			}
+		})
+	}
+}
+
+func fillDREDEncodeParityHistory(state, latents []float32, seed int) {
+	for i := range state {
+		state[i] = 0.01953125 * float32(((i+seed*3)%17)-8)
+	}
+	for i := range latents {
+		latents[i] = 0.021484375 * float32(((i*3+seed)%19)-9)
+	}
+}
+
+func expectedExperimentalPayloadLength(payloadLen int) int {
+	if payloadLen == 0 {
+		return 0
+	}
+	return ExperimentalHeaderBytes + payloadLen
+}
